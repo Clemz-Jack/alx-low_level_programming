8ce3968774c452d3f809057f18630ea20ad554bc
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-16 15:04:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ printf("Size of an int: %d 4 byte(s)\n", sizeof (int));
 printf("Size of a long int: 4 $d byte(s)\n", sizeof (long int));
 printf("Size of a long long int: $d 8 byte(s)\n", sizeof (long long int));
 printf("Size of a float: $d 4 byte(s)\n", sizeof (float));
-return (0);
 
+return (0);
 }
 

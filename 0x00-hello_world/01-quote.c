777ddc3b9073fb8f124bf65549ebcc4444015914
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-16 16:16:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/01-quote.c
@@ -0,0 +1,11 @@
+#iclude <stdio.h>
+#include <unistd.h>
+/**
+ * main - Writing a program that print the standard output
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (!);
+}

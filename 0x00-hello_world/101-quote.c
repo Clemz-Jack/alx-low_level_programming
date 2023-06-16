a95d8c39e0ac6e51f4664d0bb8f40f6613078b51
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-16 16:28:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
+
 }

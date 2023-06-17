2348969b07667b954d3992db6b6f90a1e641e121
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-17 14:48:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h>>
 /**
 * main - entry point
 *
-* return: always 0 (success)
+* return: 1 (success)
 */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,");
+	return (1);
 }
+
 

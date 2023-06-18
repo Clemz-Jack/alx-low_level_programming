a93d12145a0912dcb4417cfaecf2d071c7b35a1a
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-18 06:54:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print the alphabet
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alp[62] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; 1 < 62; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}

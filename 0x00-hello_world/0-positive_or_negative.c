cbbd51dcb9829c80d7d1f58c2d0aa6f3eac74044
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-17 17:27:32 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/0-positive_or_negative.c
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -2,19 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - determines if a number is positive, negative or zero
+*main - determines if a number is positive, negative or zero
 *
-* return: always 0 (success)
+*return: always 0 (success)
 */
 int main(void)
 {
-srand(time(0));
+srand(time(0))
 int n = rand() % 201 - 100;
 if (n > 0)
 {
 printf("positive\n");
 }
-else if (n < 0)
+else if (n == 0)
 {
 printf("negative\n");
 }

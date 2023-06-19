1645955f27ee1851cfbf7202f042b1ac1089c2f2
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-19 00:54:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	char alp[62] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[62] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 	int i;
 
 	for (i = 0; i < 62; i++)

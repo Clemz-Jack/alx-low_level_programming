f3cee91c37adfc9e1b8b57cdebfd54ab0e040d3c
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-19 08:43:38 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - print numbers betwwen 0 to 9 and letters between a to f 
+*main - print numbers betwwen 0 to 9 and letters between a to f
 *
 *Return: Always 0 (success)
 */

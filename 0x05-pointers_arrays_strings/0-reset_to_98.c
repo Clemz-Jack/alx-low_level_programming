71f1f2adad47c51acadf8ef34c51b19f6bad39ec
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-06-26 13:19:04 -0700

    0-main

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_t0_98 - function that manipulate an int
+* and updates the value it points to to 98
+* @n: input
+*
+* Return: n
+*/
+void reset_to_98(int *n);
+{
+	*n = 98;
+}

aaaddef4700146cf82a2d43b28b3fdccde791063
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-07-18 13:13:57 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,15 @@
-#ifndef DOG_H
-#define DOG_H
+fndef DOG_H
+ifdefine DOG_H
 
-/**
-* struct dog - initialize a variable of type struct dog
-* @name: name to initialize
-* @age: age to initialize
-* @owner: owner to initialize
-*/
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-#endif
+typedef struct dog 
+ { 
+         char *name; 
+         float age; 
+         char *owner; 
+ } dog_t; 
+ void init_dog(struct dog *d, char *name, float age, char *owner); 
+ void print_dog(struct dog *d); 
+ dog_t *new_dog(char *name, float age, char *owner); 
+ void free_dog(dog_t *d); 
+  
+ #endif

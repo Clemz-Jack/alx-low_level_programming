c1fc206df357d89fdeed68d5cdd251ad6224a2ce
Innocent Nkanta <innocentnkanta123@gmail.com>
2023-07-21 06:10:33 -0700

    main-opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
 /**
 * main - prints its own opcodes
 * @argc: number of arguments
 * @argv: array of arguments
-*
-* Return: Always 0 (success)
+* Return: Always 0 (Success)
 */
+
 int main(int argc, char *argv[])
+
 {
-        int bytes, i;
-        char arr;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
-        }
+	int bytes, i;
 
-        bytes = atoi(argv[1]);
- 
+	char *arr;
+
+	if (argc != 2)
+
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
-        {
-                printf("Error\n");
-                exit(2);
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
 
-        }
-
-        arr = (char *)main;
-
-        for (i = 0; i < bytes; i++)
-        {
-                if (i == bytes - 1)
-                {
-                        printf("%02hix\n", arr[i]);
-                        break;
-                }
-                printf("%02hix ", arr[i]);
-        }
-        return (0);
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
--- INSERT --                                                                     43,1          Bot
-
